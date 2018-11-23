5a35cb54d553ce6a9d2213ffd7c55f3393b45f62
Cetoo <1810653806@stud.fh-kufstein.ac.at>
2018-11-23 14:51:35 +0100

    Aufgabe6 mit Do-while Schleife muss ich noch auskommentieren!!

--- src/Aufgabe2/Aufg5WhileSchleife.java
+++ src/Aufgabe2/Aufg5WhileSchleife.java
@@ -15,26 +15,53 @@ public class Aufg5WhileSchleife {
         int zufallszahl = rnd.nextInt(max - min + 1) + min;
 
         int i = 0;
-        while (i < 3) {
-            int rueckgabewert = Integer.parseInt(JOptionPane.showInputDialog(null, "Bitte geben Sie eine Zahl zwischen 1 und 10 ein."));
-            if (rueckgabewert == zufallszahl) {
-                JOptionPane.showMessageDialog(null, "Glückwunsch! Ihre eingegebene Zahl war richtig.");
-                break;
-/*Falls die eingegebene Zahl gleich der Zufallszahl ist wird ausgegeben das die Zahl richtig erraten wurde*/
 
+        /*Aufgabe Nummer 5 While Schleife*/
+
+
+        //   while (i < 3) {
+        //       int rueckgabewert = Integer.parseInt(JOptionPane.showInputDialog(null, "Bitte geben Sie eine Zahl zwischen 1 und 10 ein."));
+        //       if (rueckgabewert == zufallszahl) {
+        //           JOptionPane.showMessageDialog(null, "Glückwunsch! Ihre eingegebene Zahl war richtig.");
+        //           break;
+        /*Falls die eingegebene Zahl gleich der Zufallszahl ist wird ausgegeben das die Zahl richtig erraten wurde*/
+
+        //       } else {
+        //           i++;
+        //           if (i > 2) {
+       // JOptionPane.showMessageDialog(null, "Keine Übereinstimmung mit der Zufallszahl " + zufallszahl + "! Alle Versuche sind aufgebraucht!");
+        /*Falls die Anzahl an Versuche 3 war und die Zahl nicht erraten wurde zeigt es an das es keine Übereinstimmung gab und was die Zufallszahl war*/
+
+        //           } else {
+        //               JOptionPane.showMessageDialog(null, "Keine Übereinstimmung mit der Zufallszahl Sie haben noch " + (3 - i) + " " +
+        //                       "Versuche übrig");
+        /*Falls der Nutzer noch übrige Versuche hat wird ihm hiermit seine Anzahl an noch übrigen Versuche angezeigt*/
+
+        //               }
+        //           }
+
+        //
+//
+
+        /*Aufgabe Nummer 6 und dafür die Aufgabe Nummer 5 darüber mit // auskommentiert*/
+
+
+        do {
+            int rueckgabewert = Integer.parseInt(JOptionPane.showInputDialog(null, "Geben Sie eine ganze Zahl zwischen 1 und 10 ein"));
+            if (rueckgabewert == zufallszahl) {
+                JOptionPane.showMessageDialog(null, "Ihre eingegebene Zahl war richtig!");
+                break;
             } else {
                 i++;
                 if (i > 2) {
-                    JOptionPane.showMessageDialog(null, "Keine Übereinstimmung mit der Zufallszahl " + zufallszahl + "! Alle Versuche sind aufgebraucht!");
-/*Falls die Anzahl an Versuche 3 war und die Zahl nicht erraten wurde zeigt es an das es keine Übereinstimmung gab und was die Zufallszahl war*/
+                    JOptionPane.showMessageDialog(null, "Es gab keine Übereinstimmung mit der Zufallszahl: " + zufallszahl + "! + Alle Versuche aufgebraucht!");
 
                 } else {
-                    JOptionPane.showMessageDialog(null, "Keine Übereinstimmung mit der Zufallszahl Sie haben noch " + (3 - i) + " " +
-                            "Versuche übrig");
-/*Falls der Nutzer noch übrige Versuche hat wird ihm hiermit seine Anzahl an noch übrigen Versuche angezeigt*/
-
+                    JOptionPane.showMessageDialog(null, "Es gab keine Übereinstimmung mit der Zufallszahl  sie haben noch " + (3 - i) + "Versuche übrig");
                 }
             }
-        }
+        } while (i < 3);
     }
 }
+
+
