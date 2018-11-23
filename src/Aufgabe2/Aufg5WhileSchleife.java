904f0d9b7d05cd5ba54e116375f71e438d0f6cc0
Cetoo <1810653806@stud.fh-kufstein.ac.at>
2018-11-23 14:21:37 +0100

    Aufgabe5 mit While Schleife

---
+++ src/Aufgabe2/Aufg5WhileSchleife.java
@@ -0,0 +1,40 @@
+package Aufgabe2;
+
+import javax.swing.*;
+import java.util.Random;
+
+public class Aufg5WhileSchleife {
+
+    public static void main(String[] args) {
+        int min = 1;
+        int max = 10;
+
+        Random rnd = new Random();
+
+
+        int zufallszahl = rnd.nextInt(max - min + 1) + min;
+
+        int i = 0;
+        while (i < 3) {
+            int rueckgabewert = Integer.parseInt(JOptionPane.showInputDialog(null, "Bitte geben Sie eine Zahl zwischen 1 und 10 ein."));
+            if (rueckgabewert == zufallszahl) {
+                JOptionPane.showMessageDialog(null, "Glückwunsch! Ihre eingegebene Zahl war richtig.");
+                break;
+/*Falls die eingegebene Zahl gleich der Zufallszahl ist wird ausgegeben das die Zahl richtig erraten wurde*/
+
+            } else {
+                i++;
+                if (i > 2) {
+                    JOptionPane.showMessageDialog(null, "Keine Übereinstimmung mit der Zufallszahl " + zufallszahl + "! Alle Versuche sind aufgebraucht!");
+/*Falls die Anzahl an Versuche 3 war und die Zahl nicht erraten wurde zeigt es an das es keine Übereinstimmung gab und was die Zufallszahl war*/
+
+                } else {
+                    JOptionPane.showMessageDialog(null, "Keine Übereinstimmung mit der Zufallszahl Sie haben noch " + (3 - i) + " " +
+                            "Versuche übrig");
+/*Falls der Nutzer noch übrige Versuche hat wird ihm hiermit seine Anzahl an noch übrigen Versuche angezeigt*/
+
+                }
+            }
+        }
+    }
+}
